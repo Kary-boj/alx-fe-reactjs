a5c72bd53342234d8424e65366f1bcc188d0a79e
Karima <karimakhady@gmail.com>
2025-02-23 21:19:00 +0000

    Added the updated file UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,20 @@
 // src/components/UserProfile.jsx
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2> {/* Display the name */}
-        <p>Age: {props.age}</p> {/* Display the age */}
-        <p>Bio: {props.bio}</p> {/* Display the bio */}
-      </div>
-    );
-  };
-  
-  export default UserProfile;
+import React, { useContext } from "react";
+import { UserContext } from "../context/UserContext"; // Make sure the path is correct
+
+const UserProfile = () => {
+  // Use the context to access the user information
+  const user = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>{user.name}</h2> {/* Display the name */}
+      <p>Age: {user.age}</p> {/* Display the age */}
+      <p>Bio: {user.bio}</p> {/* Display the bio */}
+    </div>
+  );
+};
+
+export default UserProfile;
+
   
