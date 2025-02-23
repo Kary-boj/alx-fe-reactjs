e66c1f7c671a67a5930c8ddbb2f130d511e42c31
Karima <karimakhady@gmail.com>
2025-02-23 21:10:16 +0000

    Added my company and the modified alx-react-app-props/App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -9,9 +9,13 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import { useState } from 'react';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [userData] = useState({ name: "Jane Doe", email: "jane.doe@example.com" });
 
   return (
     <>
@@ -25,6 +29,10 @@ function App() {
         age="25" 
         bio="Loves hiking and photography" 
       />
+
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
 
        <div>
         <a href="https://vite.dev" target="_blank">
