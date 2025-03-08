cd5f1a396dd4a1bb46e6a3f53cacc9f3fc19aedc
Karima <karimakhady@gmail.com>
2025-03-08 23:24:23 +0000

    Add the new folder that intergrate Fotmik and registration form using controlled components

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,19 @@
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm";
+
+function App() {
+  return (
+    <div>
+      <h1>User Registration</h1>
+      <h2>Controlled Components</h2>
+      <RegistrationForm />
+      <hr />
+      <h2>Formik Form</h2>
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
+
+
