5552d317581fa065b3789591a7e87a126ef39873
Karima <karimakhady@gmail.com>
2025-03-09 23:07:25 +0000

    Add the updated files

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,23 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
-import NotFound from "./components/NotFound";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import UserProfile from "./pages/UserProfile";
+import BlogPost from "./pages/BlogPost"; // Import BlogPost component
+import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/user/:id" element={<UserProfile />} />
         <Route path="/profile/*" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="/user/:id" element={<UserProfile />} />
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* Dynamic route for blog posts */}
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
 
