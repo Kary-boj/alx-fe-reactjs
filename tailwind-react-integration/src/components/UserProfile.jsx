f4ffb19b8da2920946178cadeceb5ce3e492ade2
Karima <karimakhady@gmail.com>
2025-03-14 21:59:37 +0000

    Update the file UserProfile.jsx with the width and the heading

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
           className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto"
         />
-        <h1 className="text-lg sm:text-xl text-blue-800 my-4">John Doe</h1>
+        <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">
+          John Doe
+        </h1>
         <p className="text-sm sm:text-base text-gray-600">
           Developer at Example Co. Loves to write code and explore new technologies.
         </p>
@@ -15,5 +17,6 @@ function UserProfile() {
   }
   
   export default UserProfile;
+  
   
   
