e7645093b00c4c60f61613c3bc820023bbdfcbd1
Karima <karimakhady@gmail.com>
2025-03-14 21:47:13 +0000

    Add the updated file and the new folder inside the src/components

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-500 text-white text-2xl font-bold">
-      Tailwind CSS is working!
+    <div className="flex items-center justify-center min-h-screen bg-blue-500">
+      <UserProfile />
     </div>
   );
 }
 
 export default App;
+
